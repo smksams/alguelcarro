5d084070edb514dc39ca11c17f1f872f9852ee78
Samuel dos Santos Garzotto Dalla Valle Filho <fsantos.samuca@gmail.com>
2024-04-11 09:48:10 -0300

    update search params

--- utils/index.ts
+++ utils/index.ts
@@ -1,12 +1,15 @@
-import { CarProps } from "@/types"
+import { CarProps, FilterProps } from "@/types"
 
-export async function fetchCars() {
+export async function fetchCars(filters: FilterProps) {
+  const {manufacturer, year, fuel, limit, model} = filters
   const headers =	{
 		'X-RapidAPI-Key': 'ef81c65b9fmshde66c5a09141e80p194f88jsnad12da8c9c96',
 		'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
 	}
 
-  const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=q3' , {
+  const url = `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`
+
+  const response = await fetch(url , {
     headers: headers,
   })
 
