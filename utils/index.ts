549ddf92a98e77ed5c577ca35de98261577de06b
Samuel Santos Garzotto Dalla Valle Filho <fsantos.samuca@gmail.com>
2024-06-03 16:22:50 -0300

    Update index.ts

--- utils/index.ts
+++ utils/index.ts
@@ -38,11 +38,11 @@ const url = new URL('https://cdn.imagin.studio/getimage')
 
 const { make, year, model } = car
 
-url.searchParams.append('customer', 'img&make')
+url.searchParams.append('customer', 'img&amp;make')
 url.searchParams.append('make', make)
 url.searchParams.append('modelFamily', model.split(" ")[0])
 url.searchParams.append('zoomType', 'fullscreen')
-url.searchParams.append('modelYear', `${year}`)
+url.searchParams.append('modelYear', `${year} || 01`)
 url.searchParams.append('angle', `${angle}`)
 
 return `${url}`
